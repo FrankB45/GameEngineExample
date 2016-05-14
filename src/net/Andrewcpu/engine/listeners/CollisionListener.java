67d7b998e7ea44be54bcaf721e094256fb2dae22
Andrew Stein <stein.andrew.01@gmail.com>
2016-05-14 12:54:19 -0400

    +Sprite Sheet +Slight Non Autonomous Physics *Entity *World [background image] +RealGM Test Examples *Frame

--- src/net/Andrewcpu/engine/listeners/CollisionListener.java
+++ src/net/Andrewcpu/engine/listeners/CollisionListener.java
@@ -6,5 +6,5 @@ import net.Andrewcpu.engine.world.Entity;
  * Created by stein on 5/10/2016.
  */
 public interface CollisionListener {
-    void onCollision(Entity e1, Entity e2);
+    void onCollision(Entity e1, Entity e2); 
 }
